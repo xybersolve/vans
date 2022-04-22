410e52d0037a08dbbc343312febd916384ff1964
xybersolve <xybersolve@gmail.com>
2022-04-21 17:54:08 -0700

    add new components files

---
+++ src/app/Task.ts
@@ -0,0 +1,8 @@
+
+export interface Task {
+  id?: number;
+  text: string;
+  complete: boolean;
+  reminder: boolean;
+  scheduled: boolean;
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,53 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: "Subfloor",
+    complete: false,
+    reminder: true,
+    scheduled: true,
+  },
+  {
+    id: 2,
+    text: "Insulation",
+    complete: false,
+    reminder: true,
+    scheduled: true
+  },
+  {
+    id: 3,
+    text: "Furring",
+    complete: false,
+    reminder: true,
+    scheduled: true
+  },
+  {
+    id: 4,
+    text: "Pull Wires",
+    complete: false,
+    reminder: true,
+    scheduled: true
+  },
+  {
+    id: 5,
+    text: "Roof Openings",
+    complete: false,
+    reminder: true,
+    scheduled: true
+  },
+  {
+    id: 6,
+    text: "Walls",
+    complete: false,
+    reminder: true,
+    scheduled: true
+  },
+  {
+    id: 7,
+    text: "Cieling",
+    complete: false,
+    reminder: true,
+    scheduled: true
+  }
+];
